133d2601d5ec605cb66cd353088cce8bba23f4f7
Brady Kelly <brady@bradykelly.net>
2017-12-26 06:14:10 +0200

    Started assessment project.

---
+++ AcmeSoft.Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AcmeSoft.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        public int PersonId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(16)]
+        public string EmployeeNum { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime EmployedDate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? TerminatedDate { get; set; }
+    }
+}
---
+++ AcmeSoft.Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcmeSoft.Models
+{
+    [Table(nameof(Person))]
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128)]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime BirthDate { get; set; }
+    }
+}
