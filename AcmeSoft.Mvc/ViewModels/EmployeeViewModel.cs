ca1d43dea134234533e52f30d909f6c1309fbcf3
Brady Kelly <brady@bradykelly.net>
2017-12-27 13:03:09 +0200

    All dates null on Create.

--- AcmeSoft.Mvc/ViewModels/EmployeeViewModel.cs
+++ AcmeSoft.Mvc/ViewModels/EmployeeViewModel.cs
@@ -30,16 +30,18 @@ namespace AcmeSoft.Mvc.ViewModels
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Date of Birth")]
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [StringLength(16)]
         [Display(Name = "Employee No.")]
         public string EmployeeNum { get; set; }
 
+        [Required]
         [Display(Name = "Date Employed")]
-        public DateTime EmployedDate { get; set; }
+        public DateTime? EmployedDate { get; set; }
 
         [Display(Name = "Date Terminated")]
         public DateTime? TerminatedDate { get; set; }
