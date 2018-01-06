7a10032cb66c4d57cfcdb6365cd5fd1175f5ecc1
Brady Kelly <brady@bradykelly.net>
2018-01-06 04:15:37 +0200

    Add person works with auto employeenum.

---
+++ AcmeSoft.Gui/ViewModels/EmploymentViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AcmeSoft.Gui.ViewModels.Base;
+using AcmeSoft.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcmeSoft.Gui.ViewModels
+{
+    public class EmploymentViewModel: BaseViewModel
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int EmployeeId { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public int PersonId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100)]
+        public string Position { get; set; }
+
+        [ReadOnly(true)]
+        public string LastName { get; set; }
+
+        [ReadOnly(true)]
+        public string IdNumber { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(16)]
+        [Display(Name = "Employee Number")]
+        public string EmployeeNum { get; set; }
+
+        [Column(TypeName = "date")]
+        [Display(Name = "Employed")]
+        public DateTime EmployedDate { get; set; }
+
+        [Column(TypeName = "date")]
+        [Display(Name = "Terminated")]
+        public DateTime? TerminatedDate { get; set; }
+
+        public Person Person { get; set; }
+    }
+}
